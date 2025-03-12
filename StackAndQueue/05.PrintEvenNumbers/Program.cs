c77f0f0895a9b6889043807a827c4cb90c58cc2e
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-12 16:29:31 +0200

    Add files via upload

---
+++ StackAndQueue/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> evenNumbers = new Queue<int>();
+
+            foreach (var num in inputNumbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evenNumbers.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",evenNumbers));
+        }
+    }
+}
