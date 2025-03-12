2ce32dd8a81fed651be4a91417043e438e7fa104
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-12 16:41:20 +0200

    Add files via upload

---
+++ StackAndQueue/06.SuperMarket/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.SuperMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> customers = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (var name in customers)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    customers.Clear();
+                }
+                else
+                {
+                    customers.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+
+            if (customers.Count > 0)
+            {
+                Console.WriteLine($"{customers.Count} people remaining.");
+            }
+        }
+    }
+}
