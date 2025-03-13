c4cb50112bdf021ef9ed997057b69be3e3b2741e
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-13 15:52:26 +0200

    Add files via upload

---
+++ StacksAndQueuesExe/5.FashionBoutique/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _5.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            int[] clothes = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int capacity = int.Parse(Console.ReadLine());
+            int fullCapacity = capacity;
+            int racks = 0;
+
+            for (int i = 0; i < clothes.Length; i++)
+            {
+                stack.Push(clothes[i]);
+            }
+            // 5 4 8 6 3 8 7 7 9
+            // 16
+
+            while (stack.Count > 0)
+            {
+                int currClothes = stack.Pop();
+                if (capacity > currClothes)
+                {
+                    capacity -= currClothes;
+                    
+                }
+                else if (capacity == currClothes)
+                {
+                    capacity -= currClothes;
+                    
+                    racks ++;
+                }
+                else
+                {
+                    racks++;
+                    capacity = fullCapacity;
+                    stack.Push(currClothes);
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
---
+++ StacksAndQueuesExe/6.SongsQueue/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace _6.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> playlist = new Queue<string>();
+            for (int i = 0; i < songs.Length; i++)
+            {
+                playlist.Enqueue(songs[i]);
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while (playlist.Count > 0)
+            {
+                string[] line = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = line[0];
+                switch (command)
+                {
+                    case "Play":
+                        playlist.Dequeue();
+                        break;
+                    case "Add":
+                        for (int i = 1; i < line.Length; i++)
+                        {
+                            if (i != line.Length - 1)
+                            {
+                                stringBuilder.Append(line[i] + " ");
+                            }
+                            else
+                            {
+                                stringBuilder.Append(line[i]);
+                            }
+                        }
+                        string song = stringBuilder.ToString();
+                        stringBuilder.Clear();
+                        if (playlist.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                        }
+                        else
+                        {
+                            playlist.Enqueue(song);
+
+                        }
+                            break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ",playlist));
+                        break;
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
