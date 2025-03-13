cd362fe810f479e5d36b22b22f808658b9ddc5f5
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-13 14:42:30 +0200

    Add files via upload

---
+++ StacksAndQueuesExe/3.MaxAndMinElement/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _3.MaxAndMinElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int commandNumber = input[0];
+                if (commandNumber == 1)
+                {
+                    int elementToPush = input[1];
+                    stack.Push(elementToPush);
+                }
+
+                else if (commandNumber == 2 && stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+
+                else if (commandNumber == 3 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Max());
+                }
+                else if (commandNumber == 4 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Min());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",stack));
+        }
+    }
+}
---
+++ StacksAndQueuesExe/4.FastFood/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _4.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFood = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int biggestOrder = 0;
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < orders.Length; i++)
+            {
+                queue.Enqueue(orders[i]);
+            }
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                int currOrder = orders[i];
+
+                if (totalFood >= currOrder)
+                {
+                    totalFood -= currOrder;
+                    queue.Dequeue();
+                    if (currOrder > biggestOrder)
+                    {
+                        biggestOrder = currOrder;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(biggestOrder);
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.Write("Orders left: ");
+                Console.Write(string.Join(", ",queue));
+            }
+        }
+    }
+}
