e0db3dd7cc55a3f2fd86946a4144b34042f0c01c
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-13 14:18:44 +0200

    basic Stack and Queue operations

---
+++ StacksAndQueuesExe/1.BasicStack/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _1.BasicStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNums = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int numbersToPush = inputNums[0];
+            int numbersToPop = inputNums[1];
+            int searchNum = inputNums[2];
+            int[] input = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(input);
+
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(searchNum))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+
+        }
+    }
+}
---
+++ StacksAndQueuesExe/2.BasicQueue/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _2.BasicQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = inputNumbers[0];
+            int s = inputNumbers[1];
+            int x = inputNumbers[2];
+
+            int[] ints = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            // 1 13 45 32 4
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(ints[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
