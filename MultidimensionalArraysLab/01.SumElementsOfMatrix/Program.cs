e5df9de76bd795ff13d413bf82b74668f31bd960
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-18 09:22:29 +0200

    Add files via upload

---
+++ MultidimensionalArraysLab/01.SumElementsOfMatrix/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _01.SumElementsOfMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowNumbers = Console.ReadLine()
+                    .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowNumbers[col];
+                }
+            }
+
+            int sumOfRows = 0;
+            int sumOfColumns = 0;
+            int sumOfAllElements = 0;
+            int currSumOfColumns = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                currSumOfColumns = 0;
+                sumOfRows++;
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    
+                    currSumOfColumns++;
+                    sumOfAllElements += matrix[row, col];
+                }
+                if (currSumOfColumns > sumOfColumns)
+                {
+                    sumOfColumns = currSumOfColumns;
+                }
+
+            }
+            Console.WriteLine(sumOfRows);
+            Console.WriteLine(sumOfColumns);
+            Console.WriteLine(sumOfAllElements);
+        }
+    }
+}
---
+++ MultidimensionalArraysLab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _02.SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] ints = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = ints[col];
+                }
+            }
+            //3, 6
+            //7 1 3 3 2 1
+            //1 3 9 8 5 6
+            //4 6 7 9 1 0
+            Queue<int> queue = new Queue<int>();
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int currSum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    currSum += matrix[row, col];
+                }
+                queue.Enqueue(currSum);
+            }
+
+            foreach (var sum in queue)
+            {
+                Console.WriteLine(sum);
+            }
+
+        }
+    }
+}
