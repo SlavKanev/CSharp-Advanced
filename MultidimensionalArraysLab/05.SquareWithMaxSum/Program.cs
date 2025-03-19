b99761b28d64f6fd61c749a48d740abacdcf3253
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-19 15:01:54 +0200

    Add files via upload

---
+++ MultidimensionalArraysLab/05.SquareWithMaxSum/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _05.SquareWithMaxSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = sizes[0];
+            int cols = sizes[1];
+
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] ints = Console.ReadLine()
+                    .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row,col] = ints[col];
+                }
+            }
+
+            //Queue<int[,]> queue = new Queue<int[,]> ();
+            int maxSum = 0;
+            int[,] currMatrix = new int[2, 2];
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int currElement = matrix[row,col];
+                    int currRightElement = matrix[row,col +1];
+                    int currDownElement = matrix[row + 1,col];
+                    int currDiagonalElement = matrix[row + 1,col +1];
+                    int currSum = currElement + currRightElement + currDownElement + currDiagonalElement;
+                    if (currSum > maxSum)
+                    {
+                        currMatrix[0,0] = currElement;
+                        currMatrix[0, 1] = currRightElement;
+                        currMatrix[1, 0] = currDownElement;
+                        currMatrix[1, 1] = currDiagonalElement;
+                        //queue.Enqueue (currMatrix);
+                        maxSum = currSum;
+                    }
+                }
+            }
+            
+            for (int row = 0; row < currMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < currMatrix.GetLength(1); col++)
+                {
+                    Console.Write(currMatrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
---
+++ MultidimensionalArraysLab/06.JaggedArrayModification/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _06.JaggedArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int rows = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] ints = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                jagged[row] = ints;
+            }
+
+            string input = Console.ReadLine();
+
+            //int rowsOfJagged = jagged.Length;
+            //int colsOfJagged = jagged[0].Length;
+
+            while (input != "END")
+            {
+                string[] inputArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string action = inputArgs[0];
+                int row = int.Parse(inputArgs[1]);
+                int col = int.Parse(inputArgs[2]);
+                int value = int.Parse(inputArgs[3]);
+                int jaggedLength = jagged.Length;
+
+                if (action == "Add")
+                {
+
+                    if (jaggedLength > row && row >= 0)
+                    {
+                        int jaggedRowLength = jagged[row].Length;
+                        if (jaggedRowLength > col && col >= 0)
+                        {
+                            jagged[row][col] += value;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+
+                }
+                else if (action == "Subtract")
+                {
+                    if (jaggedLength > row && row >= 0)
+                    {
+                        int jaggedRowLength = jagged[row].Length;
+                        if (jaggedRowLength > col && col >= 0)
+                        {
+                            jagged[row][col] -= value;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
