c246f62ebc71513a93d0de6fc5b7e2356fc4bdb9
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-19 13:59:01 +0200

    Add files via upload

---
+++ MultidimensionalArraysLab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size,size];
+            int diagonal = 0;
+            for (int row = 0; row < size; row++)
+            {
+                int[] ints = Console.ReadLine()
+                    .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row,col] = ints[col];
+                    if (row == col)
+                    {
+                        diagonal += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(diagonal);
+        }
+    }
+}
---
+++ MultidimensionalArraysLab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size,size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string line = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] += line[col];
+                    
+                }
+            }
+
+            char element = char.Parse(Console.ReadLine());
+            bool isFound = false;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    char currElement = matrix[row, col];
+                    if (element == currElement)
+                    {
+                        Console.WriteLine($"{(row, col)}");
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{element} does not occur in the matrix");
+            }
+        }
+    }
+}
