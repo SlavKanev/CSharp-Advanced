ba44fd00c83ba443841d248deb6044e44554af24
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-25 22:57:44 +0200

    Add files via upload

---
+++ SetsAndDictionaries/03.ProductShop/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.ProductShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<string, decimal>> shopSystem = new Dictionary<string, Dictionary<string, decimal>>();
+            while (input!= "Revision")
+            {
+                string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string shop = tokens[0];
+                string product = tokens[1];
+                decimal price = decimal.Parse(tokens[2]);
+                if (!shopSystem.ContainsKey(shop))
+                {
+                    shopSystem.Add(shop, new Dictionary<string, decimal>());
+                }
+                shopSystem[shop].Add(product, price);
+
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var shop in shopSystem.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{shop.Key} ->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value:f1}");
+                }
+            }
+        }
+    }
+}
---
+++ SetsAndDictionaries/04.CitiesByCountinentsAndCountries/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+
+namespace _04.CitiesByCountinentsAndCountries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> world = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!world.ContainsKey(continent))
+                {
+                    world.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!world[continent].ContainsKey(country))
+                {
+                    world[continent].Add(country, new List<string>());
+                }
+                world[continent][country].Add(city);
+            }
+            
+              //Europe Bulgaria Sofia
+             //Asia China Beijing
+             //Asia Japan Tokyo
+             //Europe Poland Warsaw
+             //Europe Germany Berlin
+             //Europe Poland Poznan
+             //Europe Bulgaria Plovdiv
+             //Africa Nigeria Abuja
+             //Asia China Shanghai
+
+
+            foreach (var continent in world)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
