ca9b85bab07786d366860cec2a81e0f44c0d7bbb
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-25 14:57:25 +0200

    Add files via upload

---
+++ SetsAndDictionaries/01.Count Same Values in Array/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double,int> dictionary = new Dictionary<double,int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double currNumber = input[i];
+
+                if (!dictionary.ContainsKey(currNumber))
+                {
+                    dictionary.Add(currNumber,1);
+                }
+                else
+                {
+                    dictionary[currNumber]++;
+                }
+            }
+            foreach (var number in dictionary)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+
+            
+            
+
+        }
+    }
+}
---
+++ SetsAndDictionaries/02.AVGStudentsGrades/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.AVGStudentsGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string,List<double>> students = new Dictionary<string,List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<double>());
+                }
+                students[name].Add(grade);
+
+            }
+            foreach (var student in students)
+            {
+               string name = student.Key;
+                List<double> studentGrades = student.Value;
+                double average = studentGrades.Average();
+
+                Console.Write($"{name} -> ");
+                foreach (var grade in studentGrades)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+                Console.Write($"(avg: {average:f2})");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
