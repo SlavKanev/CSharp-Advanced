14d88566adf7c74901ebb26a905c26f0e7c681d4
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-28 15:11:09 +0200

    Add files via upload

---
+++ SetsAndDictionariesExercise/3.PeriodicTable/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _3.PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> strings = new HashSet<string>();
+            SortedSet<string> sorted = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < input.Length; j++)
+                {
+                    string element = input[j];
+                    strings.Add(element);
+                    sorted.Add(element);
+                }
+            }
+            //Console.WriteLine("HashSet:");
+            //Console.WriteLine(string.Join(" ",strings));
+            //Console.WriteLine("Sorted:");
+            //Console.WriteLine(string.Join(" ",sorted));
+            //Console.WriteLine("HashSet after orderBy:");
+            foreach (var element in strings.OrderBy(x=>x))
+            {
+                Console.Write(element + " ");
+            }
+
+        }
+    }
+}
---
+++ SetsAndDictionariesExercise/4.EvenTimes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _4.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int,int> numbers = new Dictionary<int,int>();
+            for (int i = 0; i < n; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(element))
+                {
+                    numbers.Add(element, 0);
+                }
+                numbers[element]++;
+            }
+
+            foreach (var num in numbers)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                }
+            }
+        }
+    }
+}
