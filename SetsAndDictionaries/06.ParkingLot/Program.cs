a93049e5a2ea45717d6d2c00e0ca3b6773bf36ec
SlavKanev <84923673+SlavKanev@users.noreply.github.com>
2025-03-28 14:21:46 +0200

    Add files via upload

---
+++ SetsAndDictionaries/06.ParkingLot/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _06.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> parking = new HashSet<string>();
+            while (input!= "END")
+            {
+                string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = tokens[0];
+                string licensePlate = tokens[1];
+
+                if (direction == "IN")
+                {
+                    parking.Add(licensePlate);
+                }
+                else
+                {
+                    parking.Remove(licensePlate);
+                }
+
+                    input = Console.ReadLine();
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
